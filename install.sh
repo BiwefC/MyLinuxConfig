7f2c82770e30a1fdf56a5e1e4267697cd6e785e8
BiwefC <chw1996019@163.com>
2020-05-10 20:12:28 -0700

    Change soft link to hard link.

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
-CUR_DIR=`pwd`
-
-ln -s ${CUR_DIR}/zshrc ~/.zshrc_common
+ln zshrc ~/.zshrc_common
 touch ~/.zshrc_user
 echo "source ~/.zshrc_common" >> ~/.zshrc
 echo "source ~/.zshrc_user" >> ~/.zshrc
 
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
-ln -s ${CUR_DIR}/vimrc ~/.vimrc
+ln vimrc ~/.vimrc
 touch ~/.vimrc_user
 
